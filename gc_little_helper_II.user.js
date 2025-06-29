24c52d81206e217c47f489e7133ba68b4edaece9
DieBatzen <16579282+DieBatzen@users.noreply.github.com>
2025-06-29 18:17:36 +0200

    Reduce console errors even more

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -10810,7 +10810,7 @@ var mainGC = function() {
                                         // Fire 'dragend' event to force a search.
                                         unsafeWindow.MapSettings.Map.fire('dragend');
                                     }
-                                }, timeout_search+250);
+                                }, 2000);
                             }
                         }
                     });
